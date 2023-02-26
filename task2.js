9ba49e43b6fcd6b28b4318c737725770ec122d5e
Elena Shustova <alenyf-ka@mail.ru>
2023-02-26 21:50:49 +0300

    Errors correction

--- task2.js
+++ task2.js
@@ -1,23 +1,26 @@
 const getInterval = (arr, from, to) => {
+  errorHandling(arr, from, to);
+
+  const _from = from <= to ? from : to;
+  const _to = to <= from ? from : to;
+
+  return arr.filter((item) => item >= _from && item <= _to);
+};
+
+const isNumber = (num) => {
+  return !Number.isNaN(num) && typeof num === 'number';
+};
+
+const isNumberArray = (arr) => {
+  return Array.isArray(arr) && !(arr.some((item) => !isNumber(item)));
+}
+
+const errorHandling = (arr, from, to) => {
   const FROM_ERROR = 'В функцию getInterval были переданы невалидные параметры. Параметр from должен быть числом';
   const TO_ERROR = 'В функцию getInterval были переданы невалидные параметры. Параметр to должен быть числом';
   const ARR_ERROR = 'В функцию getInterval были переданы невалидные параметры. Параметр arr должен содержать только числовые значения';
 
-  if (typeof from !== 'number') {
-    throw new Error(FROM_ERROR);
-  }
-
-  if (typeof to !== 'number') {
-    throw new Error(TO_ERROR);
-  }
-
-  if (arr.some((item) => typeof item !== 'number')) {
-    throw new Error(ARR_ERROR);
-  }
-
-  if (from > to) {
-    return arr.slice(to, from + 1);
-  } else {
-    return arr.slice(from, to + 1);
-  }
-};
+  if (!isNumber(from)) throw new Error(FROM_ERROR);
+  if (!isNumber(to)) throw new Error(TO_ERROR);
+  if (!isNumberArray(arr)) throw new Error(ARR_ERROR);
+}
--- task4.js
+++ task4.js
@@ -1,6 +1,7 @@
 const getUniqArray = (arr) => {
   const TEXT_ERROR = 'В getUniqArray был передан невалидный параметр. Аргумент arr должен быть массивом чисел';
-  if (arr.some((item) => typeof item !== 'number')) {
+
+  if (!Array.isArray(arr) || arr.some((item) => Number.isNaN(item) || typeof item !== 'number')) {
     throw new Error(TEXT_ERROR);
   }
 
